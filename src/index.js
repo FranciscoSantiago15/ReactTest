07b06c7b6fcec7a0f6a1078fcca76a3d1c7fe448
FranciscoSantiago15 <57218261+FranciscoSantiago15@users.noreply.github.com>
2020-05-31 00:50:00 +0100

    v3
    record -> grave em s3 -> lambda vai buscar e faz transcribe

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Page from './counter';
 import App from './App';
-import Home from './uploadS3_v2';
+//import Home from './uploadS3_v2';
 //import Home from './uploadS3';
 import * as serviceWorker from './serviceWorker';
 
+
+
 ReactDOM.render(
-    <Page loginserver="https://4xfwd1debf.execute-api.us-east-1.amazonaws.com/test/login" timeserver="https://4xfwd1debf.execute-api.us-east-1.amazonaws.com/test/time"/>,
+    //<Page loginserver="https://4xfwd1debf.execute-api.us-east-1.amazonaws.com/test/login" timeserver="https://4xfwd1debf.execute-api.us-east-1.amazonaws.com/test/time"/>,
+    <App />,
   document.getElementById('root')
 );
 
